8d769949e4b93cc7f5d699aa546def017bd5201c
mixturka <165663065+fleskosss@users.noreply.github.com>
2025-06-24 03:39:38 +0300

    update last

---
+++ profile_modal.js
@@ -0,0 +1,35 @@
+const openBtn = document.querySelector('.open-btn');
+  const loginOverlay = document.getElementById('loginOverlay');
+  const loginModal = document.getElementById('loginModal');
+  const registerOverlay = document.getElementById('registerOverlay');
+  const registerModal = document.getElementById('registerModal');
+
+  const toRegisterBtn = document.getElementById('toRegister');
+  const backToLoginLink = document.getElementById('backToLogin');
+
+  openBtn.addEventListener('click', () => {
+    loginOverlay.style.display = 'flex';
+  });
+
+  // Переход на регистрацию
+  toRegisterBtn.addEventListener('click', () => {
+    loginOverlay.style.display = 'none';
+    registerOverlay.style.display = 'flex';
+  });
+
+  // Вернуться ко входу
+  backToLoginLink.addEventListener('click', (e) => {
+    e.preventDefault();
+    registerOverlay.style.display = 'none';
+    loginOverlay.style.display = 'flex';
+  });
+
+  // Закрытие при клике вне модального окна
+  window.addEventListener('click', (e) => {
+    if (e.target === loginOverlay) {
+      loginOverlay.style.display = 'none';
+    }
+    if (e.target === registerOverlay) {
+      registerOverlay.style.display = 'none';
+    }
+  });
